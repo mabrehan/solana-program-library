aa09120bde4633d6a97ee2b6735864ae05d00274
mabrehan <mabrehan13@gmail.com>
2022-10-03 13:45:12 +0400

    remove processor

--- token-lending/program/src/entrypoint.rs
+++ token-lending/program/src/entrypoint.rs
@@ -14,10 +14,10 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    if let Err(error) = processor::process_instruction(program_id, accounts, instruction_data) {
-        // catch the error so we can print it
-        error.print::<LendingError>();
-        return Err(error);
-    }
+    // if let Err(error) = processor::process_instruction(program_id, accounts, instruction_data) {
+    //     // catch the error so we can print it
+    //     error.print::<LendingError>();
+    //     return Err(error);
+    // }
     Ok(())
 }
