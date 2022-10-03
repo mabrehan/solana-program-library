16f748cb524834d001a0f818e9a112868d004fb0
mabrehan <mabrehan13@gmail.com>
2022-10-03 13:50:19 +0400

    update

--- token-lending/program/src/entrypoint.rs
+++ token-lending/program/src/entrypoint.rs
@@ -2,7 +2,7 @@
 
 #![cfg(all(target_arch = "bpf", not(feature = "no-entrypoint")))]
 
-use crate::{error::LendingError, processor};
+use crate::{error::LendingError};
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
     program_error::PrintProgramError, pubkey::Pubkey,
--- token-lending/program/src/lib.rs
+++ token-lending/program/src/lib.rs
@@ -6,7 +6,7 @@ pub mod entrypoint;
 pub mod error;
 pub mod instruction;
 pub mod math;
-pub mod processor;
+// pub mod processor;
 pub mod pyth;
 pub mod state;
 
